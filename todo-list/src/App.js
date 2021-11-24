edcb2dcdb946c123511cc96130145cc35524e59c
Moisés Santos <moisessantos@Moisess-MacBook-Pro.local>
2021-11-24 21:02:41 +0000

    chore(todo-list): Setup state

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -2,28 +2,25 @@ import React from 'react';
 import styles from './App.module.css';
 
 class App extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            todos: ["Vestir roupa", "Lavar os dentes", "Ir às compras"]
+        };
+    }
+    
     render() {
-        const list = ["Vestir roupa", "Lavar os dentes", "Ir às compras"];
-        /*
-            <ul>
-                <li>Vestir roupa</li>
-                <li>Lavar os dentes</li>
-                <li>Ir às compras</li>
-            </ul>
-        */
-
         return (
             <div className={styles.container}>
                 <h1>A minha lista de afazeres</h1>
                 <ul>
-                    {list.map((valor) => {
+                    {this.state.todos.map((valor) => {
                         return <li>{valor}</li>
                     })}
-                    <li><button onClick={
-                        function() {
-                            list.push("Apanhar o autocarro");
-                        }
-                    }>add</button></li>
+                    <li>
+                        <button onClick={}>Add</button>
+                        <button onClick={}>Delete</button>
+                    </li>
                 </ul>
             </div >
         );
